411809f30ef5a9c9784e5b1ca2b50f4a19dfdcc8
JTaksin <taksin.panngarm@gmail.com>
2025-06-22 08:37:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import pandas as pd
 st.set_page_config(page_title="Excel Multi-Filter App", layout="wide")
 st.title("📊 Excel Multi-Filter Web App")
 
-# อัปโหลดไฟล์ Excel
-uploaded_file = st.file_uploader("อัปโหลดไฟล์ Excel (.xls หรือ .xlsx)", type=["xls", "xlsx"])
 # 🔗 เพิ่มลิงก์ใต้ uploader
 st.markdown(
     '<a href="https://misweb.emc-kepler.com/modules/mis/report_plandate.php" target="_blank">🔗 เปิดรายงาน Plan Date แบบออนไลน์</a>',
     unsafe_allow_html=True
 )
+# อัปโหลดไฟล์ Excel
+uploaded_file = st.file_uploader("อัปโหลดไฟล์ Excel (.xls หรือ .xlsx)", type=["xls", "xlsx"])
 
 if uploaded_file:
     try:
